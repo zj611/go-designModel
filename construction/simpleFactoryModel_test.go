08bbf9590696cc351ed807da1e1ab7bbeb25e544
zj611 <zhangjunbit@163.com>
2021-07-05 21:55:42 +0800

    add builder model

--- construction/simpleFactoryModel_test.go
+++ construction/simpleFactoryModel_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 )
+
 //优点:实现了解耦
 //缺点:违背 "开闭原则"
 //适合:创建的对象比较少
@@ -17,11 +18,11 @@ type Product1 struct {
 	name string
 }
 
-func (p *Product1)SetName(name string)  {
+func (p *Product1) SetName(name string) {
 	p.name = name
 }
 
-func (p *Product1)GetName()string  {
+func (p *Product1) GetName() string {
 	return "Product1's name is " + p.name
 }
 
@@ -29,11 +30,11 @@ type Product2 struct {
 	name string
 }
 
-func (p *Product2)SetName(name string)  {
+func (p *Product2) SetName(name string) {
 	p.name = name
 }
 
-func (p *Product2)GetName()string  {
+func (p *Product2) GetName() string {
 	return "Product2's name is " + p.name
 }
 
@@ -41,19 +42,13 @@ type ProductType int
 
 //工厂
 type ProductFactory struct {
-
 }
 
-const  (
-	p1 ProductType = iota
-	p2
-)
-
-func (pf ProductFactory)Create(productType ProductType)Product  {
+func (pf ProductFactory) Create(productType string) Product {
 	switch productType {
-	case p1:
+	case "product_1":
 		return &Product1{}
-	case p2:
+	case "product_2":
 		return &Product2{}
 	default:
 		return nil
@@ -61,14 +56,13 @@ func (pf ProductFactory)Create(productType ProductType)Product  {
 
 }
 
-func TestSimpleFactoryModel(t *testing.T){
+func TestSimpleFactoryModel(t *testing.T) {
 	var pf ProductFactory
-	p := pf.Create(p1)
-	p.SetName("i am p1")
+	p := pf.Create("product_1")
+	p.SetName("i am product1")
 	fmt.Println(p.GetName())
 
-	p = pf.Create(p2)
-	p.SetName("i am p2")
+	p = pf.Create("product_2")
+	p.SetName("i am product2")
 	fmt.Println(p.GetName())
 }
-
